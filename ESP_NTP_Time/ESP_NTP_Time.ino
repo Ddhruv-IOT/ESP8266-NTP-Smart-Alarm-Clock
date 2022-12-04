dcf13a59abff19da63c37a332a78701559f956d2
Ddhruv-IOT <ddhruvarora2@gmail.com>
2022-12-04 16:40:56 +0530

    Added comments for better understanding

--- ESP_NTP_Time/ESP_NTP_Time.ino
+++ ESP_NTP_Time/ESP_NTP_Time.ino
@@ -3,25 +3,26 @@
 #include <WiFiUdp.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);                               // create I2C object
 
-const char *ssid = "MADT";
-const char *password = "luckydumpy";
-WiFiUDP ntpUDP;
+const char *ssid = "MADT";                                        // WIFI Name
+const char *password = "luckydumpy";                              // WIFI Password
+WiFiUDP ntpUDP;                                                   // NTP Object
 
-// NTPClient timeClient(UDP& udp, const char* poolServerName, int timeOffset (5hr 30 mins -> 19800 sec), unsigned long updateInterval);
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 19800, 60000);
+// NTPClient timeClient(UDP& udp, const char* poolServerName, int timeOffset (5hr 30 mins -> 19800 sec), unsigned long updateInterval); 
+// NTP Client Object Parameters
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 19800, 60000);       
 
 String intro[] = {"H", "E", "L", "L", "O", " ", "W", "O", "R", "L", "D"};
 String arr_days[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-String date_time;
 
 void setup() {
-  Serial.begin(115200);
-  lcd.init();
-  lcd.backlight();
-  WiFi.begin(ssid, password);
+  Serial.begin(115200);                                           // Set the correct baud rate for ESP8266
+  lcd.init();                                                     
+  lcd.backlight();                                                
+  WiFi.begin(ssid, password);                                     // Connect NodeMCU to WIFI
 
+  // Wait for WIFI to get connected
   while (WiFi.status() != WL_CONNECTED) {
     for (int i = 0; i <= 10; i++) {
       lcd.setCursor(i + 2, 0);
@@ -30,22 +31,24 @@ void setup() {
       Serial.print(".");
     }
   }
-
-  Serial.println("Connected....");
-  timeClient.begin();
+  
+  Serial.println("Connected....");                                // WIFI Connected
+  timeClient.begin();                                             // Start the NTP 
   lcd.clear();
 }
 
 void loop() {
-  timeClient.update();
+  timeClient.update();                                            // Start the update procedure
 
   Serial.println(timeClient.getFormattedTime());
   lcd.setCursor(2, 0);
-  
+
+  // Get Hours, Mins and Seconds
   int hh = timeClient.getHours();
   int mm = timeClient.getMinutes();
   int sec = timeClient.getSeconds();
-  
+
+  // Format the Output
   if (hh < 10) {
     lcd.print(0);
   }
@@ -63,6 +66,7 @@ void loop() {
   }
   lcd.print(sec);
 
+  // Translate the day and print it
   int day = timeClient.getDay();
   lcd.print(" ");
   lcd.print(arr_days[day]);
