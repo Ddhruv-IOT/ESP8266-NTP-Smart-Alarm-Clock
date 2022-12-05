1cca162004b6f323e889aeeae33f4026c011a896
Ddhruv-IOT <ddhruvarora2@gmail.com>
2022-12-05 16:54:34 +0530

    updated dependencies

--- ESP_NTP_Time_ServerControl/ESP_NTP_Time_ServerControl.ino
+++ ESP_NTP_Time_ServerControl/ESP_NTP_Time_ServerControl.ino
@@ -2,55 +2,24 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <LiquidCrystal_I2C.h>
-
-#include <Arduino.h>
-#ifdef ESP32
-  #include <WiFi.h>
-  #include <AsyncTCP.h>
-#else
-  #include <ESP8266WiFi.h>
-  #include <ESPAsyncTCP.h>
-#endif
+#include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-
-AsyncWebServer server(80);
 
 #define light 2
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+AsyncWebServer server(80);
 
 const char *ssid = "MADT";
 const char *password = "luckydumpy";
 WiFiUDP ntpUDP;
 
-const char* PARAM_INPUT_1 = "input1";
-const char* PARAM_INPUT_2 = "input2";
-const char* PARAM_INPUT_3 = "input3";
-
-// HTML web page to handle 3 input fields (input1, input2, input3)
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-  <title>ESP Input Form</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head><body>
-  <form action="/get">
-    input1: <input type="text" name="input1">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    input2: <input type="text" name="input2">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    input3: <input type="text" name="input3">
-    <input type="submit" value="Submit">
-  </form>
-</body></html>)rawliteral";
-
-
-
 String date_formatted;
 String month_formatted;
 String clean_date;
+
+const char* PARAM_INPUT_1 = "input1";
+const char* PARAM_INPUT_2 = "input2";
+const char* PARAM_INPUT_3 = "input3";
 
 // NTPClient timeClient(UDP& udp, const char* poolServerName, int timeOffset (5hr 30 mins -> 19800 sec), unsigned long updateInterval);
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 19800, 300000);
@@ -58,35 +27,46 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", 19800, 300000);
 String intro[] = {"H", "E", "L", "L", "O", " ", "W", "O", "R", "L", "D"};
 String arr_days[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
-void setup() {
-  pinMode(light, OUTPUT);
-  Serial.begin(115200);
-  lcd.init();
-  lcd.backlight();
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    for (int i = 0; i <= 10; i++) {
-      lcd.setCursor(i + 2, 0);
-      lcd.print(intro[i]);
-      delay(250);
-      Serial.print(".");
-    }
-  }
+// HTML web page to handle 3 input fields (input1, input2, input3)
+const char index_html[] PROGMEM = R"rawliteral(
+  <!DOCTYPE HTML>
+    <html>
+      <head>
+        <title>ESP Input Form</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      </head>
+      <body>
+        <form action="/get">
+          input1: <input type="text" name="input1">
+          <input type="submit" value="Submit">
+        </form>
+        <br>
+        <form action="/get">
+          input2: <input type="text" name="input2">
+          <input type="submit" value="Submit">
+        </form>
+        <br>
+        <form action="/get">
+          input3: <input type="text" name="input3">
+          <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>)rawliteral";
 
-  Serial.println("Connected....");
-  Serial.println(WiFi.localIP());
-  timeClient.begin();
-  lcd.clear();
 
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+void serverCstm() {
   // Send web page with input fields to client
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send_P(200, "text/html", index_html);
   });
 
   // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
-  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest * request) {
     String inputMessage;
     String inputParam;
     // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
@@ -109,12 +89,49 @@ void setup() {
       inputParam = "none";
     }
     Serial.println(inputMessage);
-    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
-                                     + inputParam + ") with value: " + inputMessage +
-                                     "<br><a href=\"/\">Return to Home Page</a>");
+    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field ("
+                  + inputParam + ") with value: " + inputMessage +
+                  "<br><a href=\"/\">Return to Home Page</a>");
   });
   server.onNotFound(notFound);
   server.begin();
+}
+
+
+void setup() {
+
+  Serial.begin(115200);
+  pinMode(light, OUTPUT);
+
+  lcd.init();
+  lcd.backlight();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    for (int i = 0; i <= 10; i++) {
+      lcd.setCursor(i + 2, 0);
+      lcd.print(intro[i]);
+      delay(250);
+      Serial.print(".");
+    }
+  }
+
+  Serial.println("Connected....");
+  Serial.println(WiFi.localIP());
+
+  lcd.clear();
+  lcd.setCursor(4, 0);
+  lcd.print("Your IP:");
+  lcd.setCursor(0, 1);
+  lcd.print(WiFi.localIP());
+  delay(5000);
+
+  timeClient.begin();
+  lcd.clear();
+  
+  serverCstm();
 }
 
 void loop() {
@@ -190,8 +207,4 @@ void loop() {
   else {
     digitalWrite(light, HIGH);
   }
-}
-
-void notFound(AsyncWebServerRequest *request) {
-  request->send(404, "text/plain", "Not found");
 }
